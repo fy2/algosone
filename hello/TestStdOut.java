4cc4018c7485d5f14cddc74c3ad49b2cf1a0401e
Feyruz Yalcin <feyruzyalcin@gmail.com>
2016-09-11 15:20:11 +0100

    Testing StdOut libray class...
    Had to compile and run like this:
    $ javac -cp /home/feyruz/algs4/algs4.jar TestStdOut.java
    $ java -cp /home/feyruz/algs4/algs4.jar:. TestStdOut

---
+++ hello/TestStdOut.java
@@ -0,0 +1,10 @@
+import edu.princeton.cs.algs4.StdOut;
+public class TestStdOut {
+    public static void main(String[] args) {
+        // write to stdout
+        StdOut.println("Test");
+        StdOut.println(17);
+        StdOut.println(true);
+        StdOut.printf("%.6f\n", 1.0/7.0);
+    }
+}
