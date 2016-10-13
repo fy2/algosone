bfc8fbb9894d39994d197efcbef28ea14dfea388
Feyruz Yalcin <feyruzyalcin@gmail.com>
2016-10-13 12:12:45 +0100

    Added open and isOpen implementations and tests

--- week1/Percolation.java
+++ week1/Percolation.java
@@ -7,18 +7,20 @@ public class Percolation {
   private int top;
   private int bot;
   public WeightedQuickUnionUF wquf;
+  private int size;
 
   public Percolation(int n) {
     grid = new boolean[n][n];
     wquf = new WeightedQuickUnionUF(1 + n * n + 1);
     top = 0;
     bot = n * n +1;
+    size = n;
 
 
     int pos = 0;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-          grid[i][j] = true;
+          grid[i][j] = false;
         }
     }
     for (int i = 1; i <= n; i++) {
@@ -28,19 +30,55 @@ public class Percolation {
   }
 
   public void open(int i, int j) {
-    return;
+
+    grid[i-1][j-1] = true;
+
+    int curPos = grid_coord_to_int(i, j);
+
+    int above = i - 1;
+    int below = i + 1;
+    int left  = j - 1;
+    int right = j + 1;
+
+    if (i > 1) { // only if we are not on the very first row
+      if (isOpen(above, j)) { // upper neighbour 'open'
+        wquf.union(curPos, grid_coord_to_int(above, j));
+      }
+    }
+    if (i < size) { // only if we are not on the very last row
+      if (isOpen(below, j)) { // below neighbour 'open'
+        wquf.union(curPos, grid_coord_to_int(below, j));
+      }
+    }
+
+    if (j > 1) { // only if we are not on the left edge of row
+      if (isOpen(i, left)) {
+        wquf.union(curPos, grid_coord_to_int(i, left));
+      }
+    }
+    if (j < size) {
+      if (isOpen(i, right)) {
+        wquf.union(curPos, grid_coord_to_int(i, right));
+      }
+    }
   }
 
   public boolean isOpen(int i, int j) {
-    return true;
+    return grid[i-1][j-1];
   }
 
   public boolean isFull(int i, int j) {
-    return true;
+    int coord = grid_coord_to_int(i, j);
+    return isOpen(i, j) && (wquf.connected(top, coord) || wquf.connected(bot, coord));
+  }
+
+  private int grid_coord_to_int (int i, int j) {
+    int res = ( (i - 1) * size + j );
+    return res;
   }
 
   public boolean percolates() {
-    return true;
+    return false;
 //    return this.wquf.connected(this.top_root, this.bottom_root);
   }
 }
--- week1/PercolationTest.java
+++ week1/PercolationTest.java
@@ -1,4 +1,3 @@
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.*;
 import org.junit.Test;
 import edu.princeton.cs.algs4.*;
@@ -7,6 +6,7 @@ public class PercolationTest {
   public void topRootIsConnectedToFirstRow() {
     int n = 5;
     int top = 0;
+    int bot = n * n  + 1; // 26
     Percolation perc = new Percolation(n);
     for (int j = 1; j <= 5 ; j++) {
       assertTrue(perc.wquf.connected(top, j));
@@ -14,7 +14,7 @@ public class PercolationTest {
     assertFalse(perc.wquf.connected(top, 6 ));
     assertFalse(perc.wquf.connected(top, 15));
     assertFalse(perc.wquf.connected(top, 25));
-    assertFalse(perc.wquf.connected(top, 26));
+    assertFalse(perc.wquf.connected(top, bot));
 
   }
 
@@ -30,5 +30,68 @@ public class PercolationTest {
     assertFalse(perc.wquf.connected(bot, 20));
     assertFalse(perc.wquf.connected(bot, 15));
     assertFalse(perc.wquf.connected(bot, 1));
+    assertFalse(perc.wquf.connected(bot, top));
   }
+
+  @Test
+  public void testOpen() {
+    Percolation perc = new Percolation(5);
+    assertFalse(perc.isOpen(2, 1));
+    perc.open(2,1);
+    assertTrue(perc.isOpen(2, 1));
+  }
+
+  @Test
+  public void testIsFull() {
+
+/*
+       1       2      3     4     5
+    +-------+------+-----+-----+------+
+    |   O   |      |     |     |      | 1
+    |       |      |     |     |      |
+    +---------------------------------+
+    |   O   |      |     |     |      | 2
+    |       |      |     |     |      |
+    +---------------------------------+
+    |       |      |     |     |      | 3
+    |       |      |     | O   |      |
+    +---------------------------------+
+    |       |      |     |  O  |      | 4
+    |       |      |     |     |  O   |
+    +---------------------------------+
+    |       |      |     |     |      | 5
+    |       |      |     |     |   O  |
+    +-------+------+-----+-----+------+
+*/
+
+    Percolation perc = new Percolation(5);
+    assertFalse(perc.isFull(2, 1));
+    perc.open(2,1);
+    assertFalse(perc.isFull(2, 1));
+    perc.open(1,1);
+    assertTrue(perc.isFull(1, 1));
+    assertTrue(perc.isFull(2, 1));
+
+
+    assertFalse(perc.isFull(3, 4));
+    assertFalse(perc.isFull(4, 4));
+    assertFalse(perc.isFull(4, 5));
+    assertFalse(perc.isFull(5, 5));
+    perc.open(3,4);
+    perc.open(4,4);
+    perc.open(4,5);
+    assertFalse(perc.isFull(3, 4));
+    assertFalse(perc.isFull(4, 4));
+    assertFalse(perc.isFull(4, 5));
+    assertFalse(perc.isFull(5, 5));
+
+    perc.open(5,5);
+    assertTrue(perc.isFull(3, 4));
+    assertTrue(perc.isFull(4, 4));
+    assertTrue(perc.isFull(4, 5));
+    assertTrue(perc.isFull(5, 5));
+
+
+  }
+
 }
